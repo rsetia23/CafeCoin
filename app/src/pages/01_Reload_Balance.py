3daf7c2e21b80b8d26b5bde2f906090dd0876f9d
Andrew Fielding <andrewfielding@Andrews-MacBook-Air.local>
2025-04-10 12:30:33 -0400

    testing API with streamlit

--- app/src/pages/01_Reload_Balance.py
+++ app/src/pages/01_Reload_Balance.py
@@ -8,34 +8,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plotly.express as px
 from modules.nav import SideBarLinks
+import requests
 
 # Call the SideBarLinks from the nav module in the modules directory
 SideBarLinks()
 
 # set the header of the page
-st.header('World Bank Data')
+st.header('Reload CafeCoin Account Balance')
 
-# You can access the session state to make a more customized/personalized app experience
+# Access the session state to greet the customer
 st.write(f"### Hi, {st.session_state['first_name']}.")
 
-# get the countries from the world bank data
 with st.echo(code_location='above'):
-    countries:pd.DataFrame = wb.get_countries()
+    try:
+       card_data = requests.get(f"http://api:4000/c/balanceupdate/{st.session_state['userID']}").json()
+    except:
+       st.write('Could not load cards.')
+    
+    if card_data:
+        card_options = {card['CardNumber'] : card['MethodID'] for card in card_data}
+
+        selected_card = st.selectbox('Choose one of your cards on file:', options = list(card_options.keys()))
+        st.write('Thank you for your selection!')
+    else:
+        st.write('No cards on file')
+
+    
+  #  if card_data:
+   #    card_options = {}
+    #   for card in card_data:
+     #      card_options[card['CardNumber'] : card['MethodID']]
+        
+      #  selected_card = st.selectbox("Choose one of your cards on file:", options=list(card_options.keys()))
+       
+
    
-    st.dataframe(countries)
+   
+   #countries:pd.DataFrame = wb.get_countries()
+   
+    #st.dataframe(countries)
 
 # the with statment shows the code for this block above it 
-with st.echo(code_location='above'):
-    arr = np.random.normal(1, 1, size=100)
-    test_plot, ax = plt.subplots()
-    ax.hist(arr, bins=20)
+# with st.echo(code_location='above'):
+#     arr = np.random.normal(1, 1, size=100)
+#     test_plot, ax = plt.subplots()
+#     ax.hist(arr, bins=20)
 
-    st.pyplot(test_plot)
+#     st.pyplot(test_plot)
 
 
-with st.echo(code_location='above'):
-    slim_countries = countries[countries['incomeLevel'] != 'Aggregates']
-    data_crosstab = pd.crosstab(slim_countries['region'], 
-                                slim_countries['incomeLevel'],  
-                                margins = False) 
-    st.table(data_crosstab)
+# with st.echo(code_location='above'):
+#     slim_countries = countries[countries['incomeLevel'] != 'Aggregates']
+#     data_crosstab = pd.crosstab(slim_countries['region'], 
+#                                 slim_countries['incomeLevel'],  
+#                                 margins = False) 
+#     st.table(data_crosstab)
