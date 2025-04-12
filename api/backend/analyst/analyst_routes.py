0db739ae53df28f6fc10edda44714245c7b499ea
Josh-Len <len.j@northeastern.edu>
2025-04-12 00:52:09 -0400

    adding analyst routes

--- api/backend/analyst/analyst_routes.py
+++ api/backend/analyst/analyst_routes.py
@@ -11,7 +11,18 @@ analyst_bp = Blueprint('analyst', __name__)
 def get_analyst_transactions():
     current_app.logger.info('GET /transactions route')
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT TransactionID FROM Transactions')
+    cursor.execute('SELECT TransactionID, CustomerID, MerchantID, PaymentMethod, CardUsed, TransactionType, AmountPaid FROM Transactions')
+    theData = cursor.fetchall()
+    the_response = make_response(theData)
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+@analyst_bp.route('/orderdetails', methods=['GET'])
+def get_analyst_transactions():
+    current_app.logger.info('GET /orderdetails route')
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT OrderItemNum, TransactionID, ItemID, Price, RewardRedeemed, Discount FROM OrderDetails')
     theData = cursor.fetchall()
     the_response = make_response(theData)
     the_response.status_code = 200
